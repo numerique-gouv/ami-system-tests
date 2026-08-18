c451c05875136e88ba0ea3b8fdee251f147b8e80
Nicolas <nicolas.fedou@gmail.com>
2026-08-18 17:11:10 +0200

    utilise les définitions de users

---
+++ src/helpers/test-users.local.ts
@@ -0,0 +1,10 @@
+export const credentials = {
+  avec_nom_dusage: {
+    password: '123',
+    fcHash: 'b09ba1d3248ce7dcaf159b271923545bc5ea977ff50919e5244861cfdf4b2ddb',
+  },
+  test: {
+    password: '123',
+    fcHash: '4abd71ec1f581dce2ea2221cbeac7c973c6aea7bcb835acdfe7d6494f1528060',
+  },
+}
